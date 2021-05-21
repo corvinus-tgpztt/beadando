b47a8f46315fafd80d2ebd46e0da49741e4fd92f
corvinus-tgpztt <mate.szladek@stud.uni-corvinus.hu>
2021-05-21 20:11:14 +0200

    alap oldal létrejött

--- Beadando/Beadando/Startup.cs
+++ Beadando/Beadando/Startup.cs
@@ -33,24 +33,21 @@ namespace Beadando
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles(); //A sorrend fontos!
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
